e5b11f8f961802916edac11877717fd30657ec62
MOUAD-TALHAOUI <talhaouimouad02@gmail.com>
2021-06-14 17:28:33 +0100

    add friend and remove fonctionality

--- app/src/main/java/com/mouad/trackapp/Adapter/FriendAdapter.java
+++ app/src/main/java/com/mouad/trackapp/Adapter/FriendAdapter.java
@@ -14,29 +14,19 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.mouad.trackapp.MainActivity;
 import com.mouad.trackapp.MessageActivity;
+import com.mouad.trackapp.Model.Friend;
 import com.mouad.trackapp.Model.User;
 import com.mouad.trackapp.R;
-import com.mouad.trackapp.RegisterActivity;
 
-import java.lang.ref.Reference;
-import java.util.HashMap;
 import java.util.List;
 
-public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder> {
     private Context mContext;
-    private List<User> mUsers;
+    private List<Friend> mUsers;
     private Boolean ischat;
 
-    public UserAdapter(Context mContext,List<User> mUsers,Boolean ischat){
+    public FriendAdapter(Context mContext,List<Friend> mUsers,Boolean ischat){
         this.mContext=mContext;
         this.mUsers=mUsers;
         this.ischat=ischat;
@@ -48,13 +38,13 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(mContext).inflate(R.layout.user_item,parent,false);
-        return new UserAdapter.ViewHolder(view);
+        View view= LayoutInflater.from(mContext).inflate(R.layout.friend_item,parent,false);
+        return new FriendAdapter.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        User user=mUsers.get(position);
+        Friend user=mUsers.get(position);
         holder.username.setText(user.getUsername());
         if(user.getImageURL().equals("default")){
             holder.profile_image.setImageResource(R.mipmap.ic_launcher);
@@ -81,67 +71,13 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
             holder.img_on.setVisibility(View.GONE);
         }
 
-        FirebaseUser fuser1= FirebaseAuth.getInstance().getCurrentUser();
-
-
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference().child("Friends").child(fuser1.getUid());
-        ((DatabaseReference) rootRef).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                if (snapshot.hasChild(user.getId())) {
-                    holder.remove_friend.setVisibility(View.VISIBLE);
-                    holder.add_friend.setVisibility(View.GONE);
-                }else{
-                    holder.remove_friend.setVisibility(View.GONE);
-                    holder.add_friend.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-        holder.add_friend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-
-                DatabaseReference reference=FirebaseDatabase.getInstance().getReference();
-                HashMap<String,String> hashMap=new HashMap<>();
-                hashMap.put("id",user.getId());
-                hashMap.put("email",user.getEmail());
-                hashMap.put("username",user.getUsername());
-                hashMap.put("status",user.getStatus());
-                hashMap.put("imageURL",user.getImageURL());
-
-                reference.child("Friends").child(fuser1.getUid()).child(user.getId()).setValue(hashMap).addOnCompleteListener(task1 -> {
-                            if (task1.isSuccessful()) {
-                                //Toast.makeText(null,"added",Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                );
-
-
-
-            }
-        });
-
-        holder.remove_friend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                DatabaseReference reference=FirebaseDatabase.getInstance().getReference();
 
 
 
 
-                reference.child("Friends").child(fuser1.getUid()).child(user.getId()).removeValue();
 
-            }
-        });
+
+
 
 
 
---
+++ app/src/main/java/com/mouad/trackapp/Model/Friend.java
@@ -0,0 +1,63 @@
+package com.mouad.trackapp.Model;
+
+public class Friend {
+    private String id;
+    private String username;
+    private String imageURL;
+    private String email;
+    private String status;
+
+
+
+    public Friend(String id, String username, String imageURL,String email,String status) {
+        this.id = id;
+        this.username = username;
+        this.imageURL = imageURL;
+        this.email = email;
+        this.status = status;
+    }
+
+    public Friend() {
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
+
